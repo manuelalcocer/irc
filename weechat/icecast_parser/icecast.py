20394cea1d4a3da08ba76a0e87cbdefa1e936e22
manuelalcocer <m.alcocer1978@gmail.com>
2015-12-31 20:21:30 +0100

    First version operative

--- weechat/icecast_parser/icecast.py
+++ weechat/icecast_parser/icecast.py
@@ -10,6 +10,7 @@
 import sys
 reload(sys)
 sys.setdefaultencoding("utf8")
+    
 
 SCRIPT_NAME = 'icecast'
 SCRIPT_AUTHOR = 'nashgul'
@@ -36,65 +37,84 @@ except ImportError as message:
     print 'Missing package(s) for %s: %s' % (SCRIPT_NAME, message)
     import_ok = False
 
-def fetch_vars():
-    webpage = config_values['url']
-    return webpage
-
-def icecast_cmd_cb(data_ptr, buffer_exec, args):
-    webpage = fetch_vars()
-    try:
-        status_page = urlopen(webpage)
-        radio_online = True
-    except:
-        radio_online = False
-    if radio_online:
-        status_dict = loads(status_page.read())
-        global streams_list
-        streams_list = status_dict['server']['streams']
-        if streams_list:
-            # mounts available
-            show_stats()
-        else:
-            # no mounts available
-            pass
-    return weechat.WEECHAT_RC_OK
-
-def show_stats():
-    pass
-
 def initialize_config(name):
     global config
-    global config_file
     config = {}
+
+    global config_file
     config_file = weechat.config_new(name, 'my_config_reload_cb', '')
+
+    global config_values
+    config_values = {}
+
+    global config_keys
+    config_keys = {
+            'strings' : ['url', 'streams', 'stream_options'],
+            'integers' : ['time_delay']
+            }
+
     if not config_file:
         return
+
     section = weechat.config_new_section(config_file, 'config', 0,0, '', '', '', '', '', '', '', '', '', '')
     config['url'] = weechat.config_new_option(config_file, section,
             'icecast_json_url', 'string', 'http directory for /status-json.xsl',
             '', 0, 0, '', '', 1,
             '', '',
-            'load_vars_cb', '',
+            'load_str_vars_cb', '',
             '', '')
     config['streams'] = weechat.config_new_option(config_file, section,
             'streams', 'string', 'mounts to show (without slash and separated by commas)',
             '', 0, 0, '', '', 1,
             'check_mounts_cb', '',
-            'load_vars_cb', '',
+            'load_str_vars_cb', '',
+            '', '')
+    config['stream_options'] = weechat.config_new_option(config_file, section,
+            'stream_options', 'string', 'stream options to show',
+            '', 0, 0,
+            'artist,title,listeners', 'artist,title,listeners', 1,
+            '', '',
+            'load_str_vars_cb', '',
+            '', '')
+    config['time_delay'] = weechat.config_new_option(config_file, section,
+            'time_delay', 'integer', 'time delay between info show when streams > 1',
+            '', 0, 0, '', '', 1,
+            '', '',
+            'load_int_vars_cb', '',
             '', '')
 
 def check_mounts_cb(data_ptr, option_ptr, new_value):
+    # checks if mount list contains spaces
+    if len(new_value.split()) > 1:
+        # error if mounts contain space
+        return 0
+
+    # prepares to check individually
     if ',' in new_value:
         mounts = new_value.split(',')
-        for mount in mounts:
-            if mount.startswith('/') or mount.endswith('/'):
-                return 0
-        return 1
+    else:
+        mounts = new_value
+    
+    # checks mounts individually
+    for mount in mounts:
+        if mount.startswith('/') or mount.endswith('/'):
+            # error if mount contains slashes
+            return 0
+        elif len(mount.split()) > 1:
+            # error if mount contains space
+            return 0
+    # return ok if no errors in mount list
+    return 1
 
-def load_vars_cb(data_ptr, option_ptr):
-    global config_values
-    config_values = {}
-    for key, pointer in config.items():
+def load_str_vars_cb(data_ptr, option_ptr):
+    for key in config_keys['strings']:
+        pointer = config[key]
+        config_values[key] = weechat.config_string(pointer)
+    return weechat.WEECHAT_RC_OK
+
+def load_int_vars_cb(data_ptr, option_ptr):
+    for key in config_keys['integer']:
+        pointer = config[key]
         config_values[key] = weechat.config_string(pointer)
     return weechat.WEECHAT_RC_OK
 
@@ -103,6 +123,41 @@ def my_config_reload_cb():
 
 def config_read():
     return weechat.config_read(config_file)
+
+def icecast_cmd_cb(data_ptr, buffer_exec, args):
+    try:
+        status_page = urlopen(config_values['url'])
+        radio_online = True
+    except:
+        radio_online = False
+    if radio_online:
+        status_dict = loads(status_page.read())
+        global streams_dict
+        streams_dict = status_dict['server']['streams'].copy()
+        if len(streams_dict) >= 1:
+            # mounts available
+            show_stats(buffer_exec, streams_dict)
+        else:
+            # no mounts available
+            no_mounts_message = 'No hay radios disponibles'
+            weechat.command(buffer_exec, '/print %s' % no_mounts_message)
+    return weechat.WEECHAT_RC_OK
+
+def show_stats(buffer_exec, streams_dict):
+    for mount in streams_dict.keys():
+        if mount.lstrip('/') in config_values['streams']:
+            info = create_string(mount)
+            weechat.command(buffer_exec, '/print %s' % info)
+
+def create_string(mount):
+    counter = 0
+    for option in config_values['stream_options'].split(','):
+        if counter != 0:
+            string = string + ' - ' + streams_dict.get(mount)[option]
+        else:
+            string = streams_dict.get(mount)[option]
+        counter += 1
+    return string
 
 if __name__ == '__main__' and import_ok:
     weechat.register(SCRIPT_NAME, SCRIPT_AUTHOR, SCRIPT_VERSION, SCRIPT_LICENSE, SCRIPT_LICENSE, '', '')
